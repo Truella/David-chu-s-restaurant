6615a6e294fb07ebc70fa1170d3ef5aad4aa0389
Truella <101476258+Truella@users.noreply.github.com>
2023-05-25 20:05:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let dropDown = document.getElementById("drop-down");
+let navBar = document.getElementById("navBar");
+dropDown.addEventListener("click", function(){
+    navBar.classList.toggle("hidden-nav")
+})
